2dca19497b02e92f0d2932a892b7f0d65cfe1585
Hugo Cardoso <h.cardoso@runtime-revolution.com>
2021-02-26 17:36:59 +0000

    cleanup

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 
 import { NavBar, Loading } from "./components";
-import { Home, Profile, ExternalApi } from "./views";
+import { Home, Profile } from "./views";
 import ProtectedRoute from "./auth/protected-route";
 
 import "./app.css";
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,4 @@
-import HomeContent from "./home-content";
-import Footer from "./footer";
-import Hero from "./hero";
 import Loading from "./loading";
 import NavBar from "./nav-bar";
 
-export { HomeContent, Footer, Hero, Loading, NavBar };
+export {Loading, NavBar };
