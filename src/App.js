1fd30a1ba39fc5ea5bae0a846f927537195afcde
Hugo Cardoso <h.cardoso@runtime-revolution.com>
2021-02-26 17:24:08 +0000

    comments and profile

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 
-import { NavBar, Footer, Loading } from "./components";
+import { NavBar, Loading } from "./components";
 import { Home, Profile, ExternalApi } from "./views";
 import ProtectedRoute from "./auth/protected-route";
 
@@ -23,7 +23,6 @@ const App = () => {
           <Switch>
             <Route path="/" exact component={Home} />
             <ProtectedRoute path="/profile" component={Profile} />
-            <ProtectedRoute path="/external-api" component={ExternalApi} />
           </Switch>
         </div>
       </div>
